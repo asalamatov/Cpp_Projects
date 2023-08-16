f0c112f35d3bc98fbc6086d547acdb6530793a98
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-08-15 19:22:14 -0500

    started julka problem

---
+++ CP_Third_Milestone/BigIntegers/julka.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void divider(int *N, int &k){
+    int x =
+}
+
+int main()
+{
+    string N, k;
+    cin >> N >> k;
+    divider(N,k);
+    return 0;
+}
